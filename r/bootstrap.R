b51bc548d56b6284304f678ab55e9ff9f3e41417
Josiah Parry <josiah.parry@gmail.com>
2025-12-20 11:42:40 -0800

    workspace

--- r/bootstrap.R
+++ r/bootstrap.R
@@ -1,3 +1,19 @@
+# see https://github.com/r-lib/pkgbuild/pull/157
+if (!requireNamespace("tomledit")) {
+  error("bootstrap.R requires `tomledit` to be installed.")
+}
+
+if (!requireNamespace("fs")) {
+  error("bootstrap.R requires `fs` to be installed.")
+}
+
+if (!requireNamespace("rextendr")) {
+  error("bootstrap.R requires `fs` to be installed.")
+}
+
+
+# Logging utils ----------------------------------------------------------
+
 .style <- function(text, code) {
   if (Sys.getenv("TERM") != "dumb") {
     paste0("\033[", code, "m", text, "\033[0m")
@@ -25,10 +41,6 @@ error <- function(...) {
 
 info("Bootstrapping rust dependencies")
 
-# see https://github.com/r-lib/pkgbuild/pull/157
-if (!requireNamespace("tomledit")) {
-  error("bootstrap.R requires `tomledit` to be installed.")
-}
 
 library(tomledit)
 
@@ -38,7 +50,6 @@ info("Reading Cargo.toml")
 cargo_path <- "src/rust/Cargo.toml"
 cargo_toml <- read_toml(cargo_path)
 crate_name <- get_item(cargo_toml, c("package", "name"))
-
 
 info("Fetching cargo metadata")
 # get the pkg metadata as a list from cargo metadata
@@ -66,19 +77,29 @@ workspace_toml <- read_toml(
 # extract workspace specific metadata from the manifest path
 workspace_fields <- get_item(workspace_toml, "workspace")
 
-# workspace members that are dependencies
-workspace_member_deps <- pkg_metadata$packages$name[which(
+
+# workspace members that are deps index:
+workspace_idx <- which(
   pkg_metadata$packages$name %in% names(pkg_deps)
-)]
+)
+
+# get the member diirectory without absolute path
+workspace_member_dep_paths <- basename(
+  gsub(
+    pkg_metadata$workspace_root,
+    "",
+    dirname(pkg_metadata$packages$manifest_path[workspace_idx])
+  )
+)
 
 # "." is the R package workspace info
-workspace_fields$members <- c(".", workspace_member_deps)
+workspace_fields$members <- c(".", workspace_member_dep_paths)
 
 # iterate through deps and copy them to the workspace
 for (.dep in non_reg_deps$path) {
   dest <- file.path("src/rust", basename(.dep))
   info("Copying dep", .dep, " into ", dest)
-  fs::dir_copy(.dep, dest)
+  fs::dir_copy(.dep, dest, overwrite = TRUE)
 }
 
 # insert the workspace settings into the R package's Cargo.toml
@@ -87,6 +108,13 @@ new_cargo_toml <- insert_items(
   cargo_toml,
   workspace = workspace_fields
 )
+
+# Remove workspace inheritance from package section since this file IS the workspace root
+pkg_section <- get_item(new_cargo_toml, "package")
+pkg_section$version <- workspace_fields$package$version
+pkg_section$edition <- workspace_fields$package$edition
+pkg_section$`rust-version` <- workspace_fields$package$`rust-version`
+new_cargo_toml <- insert_items(new_cargo_toml, package = pkg_section)
 
 # if the dependency is in the workspace members
 # we need to set workspace = true and remove path if possible
